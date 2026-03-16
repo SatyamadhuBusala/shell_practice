47073805d938b7f77f8516ded5520157de358caa
SatyamadhuBusala <madhubusala111@gmail.com>
2026-03-16 10:25:37 +0530

    3

--- if_condition.sh
+++ if_condition.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NUMBER=$1
 if [ $NUMBER -lt 10 ]; then
-    echo "given number $NUMBER is less than 10"
+    echo "given number $NUMBER is le ss than 10"
 else
     echo "given number $NUMBER is greater than or equal to 10"
 fi
