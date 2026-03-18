d6ebda52f15eada8e9d6460faf71e312ebe650c2
SatyamadhuBusala <madhubusala111@gmail.com>
2026-03-18 19:41:20 +0530

    1

---
+++ if-else.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+NUMBER=$1
+if [ $NUMBER -lt 10 ]; then
+    echo "Given number $NUMBER is less than 10"
+elif [ $NUMBER -eq 10 ]; then
+    echo "Given number $NUMBER is equal to 10"
+elif [ $NUMBER -gt 10 ]; then
+    echo "Given number $NUMBER is grater then 10"
+fi
